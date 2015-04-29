adbde6610c7dad2a825b855f379517c8145d0733
houfeng.hf <houfeng.hf@alibaba-inc.com>
2015-04-29 12:32:42 +0800

    format js

--- index.js
+++ index.js
@@ -2,75 +2,75 @@
 
 var co = require('mini-co');
 
-module.exports = function (verb) {
-  verb = verb || {};
+module.exports = function(verb) {
+    verb = verb || {};
 
-  for (var name in verb) {
-    verb[name] = function (factory) {
-      return function () {
-        return this.use(factory.apply(null, arguments));
-      };
-    }(verb[name]);
-  }
-
-  function wrap(fn, parent) {
-    var node = Object.create(verb),
-      sibling,
-      child;
-
-    fn = fn || function (next, done) {
-      next(done);
-    };
-
-    if (fn.constructor.name === 'GeneratorFunction') {
-      fn = co(fn);
+    for (var name in verb) {
+        verb[name] = function(factory) {
+            return function() {
+                return this.use(factory.apply(null, arguments));
+            };
+        }(verb[name]);
     }
 
-    node.use = function (fn) {
-      return (sibling = wrap(fn, parent));
-    };
+    function wrap(fn, parent) {
+        var node = Object.create(verb),
+          sibling,
+          child;
 
-    node.run = function (context, callback) {
-      var deep = false,
-
-        next = function (callback) {
-          var node = child || sibling;
-
-          deep = true;
-
-          if (node) {
-            node.run(context, callback);
-          } else {
-            callback.call(context, null);
-          }
-        },
-
-        done = function (err) {
-          if (!err && !deep && child && sibling) {
-            sibling.run(context, callback);
-          } else if (callback) {
-            callback.call(context, err);
-          }
+        fn = fn || function(next, done) {
+            next(done);
         };
 
-      fn.call(context, next, done);
-    };
-
-    Object.defineProperties(node, {
-      then: {
-        get: function () {
-          return (child = wrap(null, node));
+        if (fn.constructor.name === 'GeneratorFunction') {
+            fn = co(fn);
         }
-      },
-      end: {
-        get: function () {
-          return parent || node;
-        }
-      }
-    });
 
-    return node;
-  }
+        node.use = function(fn) {
+            return (sibling = wrap(fn, parent));
+        };
 
-  return wrap();
+        node.run = function(context, callback) {
+            var deep = false,
+
+        next = function(callback) {
+            var node = child || sibling;
+
+            deep = true;
+
+            if (node) {
+                node.run(context, callback);
+            } else {
+                callback.call(context, null);
+            }
+        },
+
+        done = function(err) {
+            if (!err && !deep && child && sibling) {
+                sibling.run(context, callback);
+            } else if (callback) {
+                callback.call(context, err);
+            }
+        };
+
+            fn.call(context, next, done);
+        };
+
+        Object.defineProperties(node, {
+            then: {
+                get: function() {
+                    return (child = wrap(null, node));
+                }
+            },
+            end: {
+                get: function() {
+                    return parent || node;
+                }
+            }
+        });
+
+        return node;
+    }
+
+    return wrap();
 };
--- test/milu-spec.js
+++ test/milu-spec.js
@@ -3,197 +3,202 @@
 var expect = require('chai').expect;
 var milu = require('../index');
 
-describe('milu unit test',function (){
+describe('milu unit test', function() {
 
-  describe('context',function (){
+    describe('context', function() {
 
-    it('show execute use function with correct context ',function (done){
-      var root = milu();
-      var context = {};
-      root.use(function *(next){
-        expect(this).to.equal(context);
-      });
-      root.run(context,done);
+        it('show execute use function with correct context ', function(done) {
+            var root = milu();
+            var context = {};
+            root.use(function*(next) {
+                expect(this).to.equal(context);
+            });
+            root.run(context, done);
+        });
+
+
+        it('show execute custom function with correct context ', function(done) {
+            var context = {};
+            var verb = {
+                say: function(type) {
+                    return function*(next) {
+                        expect(this).to.equal(context);
+                        yield next;
+                    }
+                }
+            };
+            var root = milu(verb);
+            root.say();
+            root.run(context, done);
+        });
+
+
+        it('show execute callback function with correct context ', function(done) {
+            var root = milu();
+            var context = {};
+            root.use(function*(next) {});
+            root.run(context, function(err) {
+                expect(this).to.equal(context);
+                done();
+            });
+        });
+
     });
 
+    describe('exception', function() {
 
-    it('show execute custom function with correct context ',function (done){
-      var context = {};
-      var verb = {
-        say: function (type){
-          return function *(next){
-            expect(this).to.equal(context);
-            yield next;
-          }
-        }
-      };
-      var root = milu(verb);
-      root.say();
-      root.run(context,done);
+        it('show execute callback function without error ', function(done) {
+            var root = milu();
+            root.use(function*(next) {
+                yield next;
+            });
+
+            root.run({}, function(err) {
+                expect(err).to.equal(null);
+                done();
+            });
+        });
+
+        it('show transmit an error object to callback', function(done) {
+            var root = milu();
+            var error = new Error('err');
+            root.use(function*(next) {
+                throw error;
+            });
+
+            root.run({}, function(err) {
+                expect(err).to.equal(error);
+                done();
+            });
+        });
+
+        it('show catch an error object in previous use function ', function(done) {
+            var root = milu();
+            var error = new Error('err');
+            root.use(function*(next) {
+                try {
+                    yield next;
+                } catch (e) {
+                    expect(e).to.equal(error);
+                }
+            }).use(function*(next) {
+                yield next;
+            }).use(function*(next) {
+                throw error;
+            });
+            root.run({}, function(err) {
+                expect(err).to.equal(null);
+                done();
+            });
+        });
+
     });
 
+    describe('use', function() {
 
-    it('show execute callback function with correct context ',function (done){
-      var root = milu();
-      var context = {};
-      root.use(function *(next){});
-      root.run(context,function (err){
-        expect(this).to.equal(context);
-        done();
-      });
+        it('should stop chain when don\'t using yield ', function(done) {
+            var root = milu();
+            var count = 'a';
+            root.use(function*(next) {
+                count += 'b';
+                yield next;
+            }).use(function*(next) {
+                count += 'c';
+            }).use(function*(next) {
+                count += 'd';
+            });
+
+            root.run({}, function(err) {
+                expect(count).to.equal('abc');
+                done();
+            });
+        });
+
+        it('should execute correct branch (single-level tree)', function(done) {
+            var verb = {
+                is: function(type) {
+                    return function*(next) {
+                        if (this.type === type) {
+                            yield next;
+                        }
+                    };
+                },
+                say: function(word) {
+                    return function*(next) {
+                        this.result = word;
+                    };
+                }
+            };
+            var root = milu(verb);
+            root.is('cat').then
+                .say('miao')
+                .end
+                .is('dog').then
+                .say('wang')
+                .end;
+
+            root.run({
+                type: 'dog'
+            }, function(err) {
+                expect(this.result).to.equal('wang');
+                done();
+            });
+        });
+
+
+        it('should execute correct branch (multi-level tree)', function(done) {
+            var verb = {
+                is: function(type) {
+                    return function*(next) {
+                        if (this.type === type) {
+                            yield next;
+                        }
+                    };
+                },
+                say: function(word) {
+                    return function*(next) {
+                        this.result = word;
+                        yield next;
+                    };
+                },
+                can: function(skill) {
+                    return function*(next) {
+                        if (this.skill === skill) {
+                            yield next;
+                        }
+                    };
+                },
+                action: function(playing) {
+                    return function*(next) {
+                        this.playing = playing;
+                    }
+                }
+            };
+            var root = milu(verb);
+            root.is('cat').then
+                .say('miao')
+                .end
+                .is('dog').then
+                .say('wang')
+                .can('swim').then
+                .action('go to swimming')
+                .end
+                .can('bike')
+                .action('ride bike')
+                .end
+                .end;
+
+            root.run({
+                type: 'dog',
+                skill: 'bike'
+            }, function(err) {
+                expect(this.result).to.equal('wang');
+                expect(this.playing).to.equal('ride bike');
+                done();
+            });
+        });
+
     });
-
-  });
-
-  describe('exception',function (){
-
-    it('show execute callback function without error ',function (done){
-      var root = milu();
-      root.use(function *(next){
-        yield next;
-      });
-
-      root.run({},function (err){
-        expect(err).to.equal(null);
-        done();
-      });
-    });
-
-    it('show transmit an error object to callback',function (done){
-      var root = milu();
-      var error = new Error('err') ;
-      root.use(function *(next){
-        throw error;
-      });
-
-      root.run({},function (err){
-        expect(err).to.equal(error);
-        done();
-      });
-    });
-
-    it('show catch an error object in previous use function ',function (done){
-      var root = milu();
-      var error = new Error('err') ;
-      root.use(function *(next){
-        try{
-          yield next;
-        }catch(e){
-          expect(e).to.equal(error);
-        }
-      }).use(function *(next){
-        yield next;
-      }).use(function *(next){
-        throw error;
-      });
-      root.run({},function (err){
-        expect(err).to.equal(null);
-        done();
-      });
-    });
-
-  });
-
-  describe('use',function (){
-
-    it('should stop chain when don\'t using yield ',function (done){
-      var root = milu();
-      var count = 'a';
-      root.use(function *(next){
-        count += 'b';
-        yield next;
-      }).use(function *(next){
-        count += 'c';
-      }).use(function *(next){
-        count += 'd';
-      });
-
-      root.run({},function (err){
-        expect(count).to.equal('abc');
-        done();
-      });
-    });
-
-    it('should execute correct branch (single-level tree)',function (done){
-      var verb = {
-        is: function (type) {
-          return function *(next) {
-            if (this.type === type) {
-              yield next;
-            }
-          };
-        },
-        say: function (word) {
-          return function *(next) {
-            this.result = word;
-          };
-        }
-      };
-      var root = milu(verb);
-      root.is('cat').then
-        .say('miao')
-        .end
-      .is('dog').then
-        .say('wang')
-        .end;
-
-      root.run({type:'dog'},function (err){
-        expect(this.result).to.equal('wang');
-        done();
-      });
-    });
-
-
-    it('should execute correct branch (multi-level tree)',function (done){
-      var verb = {
-        is: function (type) {
-          return function *(next) {
-            if (this.type === type) {
-              yield next;
-            }
-          };
-        },
-        say: function (word) {
-          return function *(next) {
-            this.result = word;
-            yield next;
-          };
-        },
-        can:function (skill){
-          return function *(next){
-            if(this.skill === skill) {
-              yield next;
-            }
-          };
-        },
-        action:function (playing){
-          return function *(next){
-            this.playing = playing;
-          }
-        }
-      };
-      var root = milu(verb);
-      root.is('cat').then
-        .say('miao')
-        .end
-      .is('dog').then
-        .say('wang')
-        .can('swim').then
-          .action('go to swimming')
-          .end
-        .can('bike')
-          .action('ride bike')
-          .end
-        .end;
-
-      root.run({type:'dog',skill:'bike'},function (err){
-        expect(this.result).to.equal('wang');
-        expect(this.playing).to.equal('ride bike');
-        done();
-      });
-    });
-
-  });
 
 
 });
